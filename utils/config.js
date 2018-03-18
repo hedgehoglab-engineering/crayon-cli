eb7f0e93ecf65cae241cdd22edd6cd5130f4b7db
Sam Turrell <sam.turrell@netsells.co.uk>
2018-03-18 22:42:02 +0000

    tdd Adds JSDoc to camelCase function

--- utils/config.js
+++ utils/config.js
@@ -1,13 +1,16 @@
 const { existsSync, readFileSync } = require('fs');
 const path = require('path');
 
+// Default config
 let config = {
     js_directory: path.resolve('./resources/assets/js'),
 };
 
+// Override with the custom config if exists
 if (existsSync(`${process.cwd()}/.crayonrc`)) {
     let projectConfig = readFileSync('./.crayonrc', 'utf8');
 
+    // Merge!
     Object.assign(config, JSON.parse(projectConfig));
 }
 
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,14 @@
+/**
+ * Convert a string to upper camelcase
+ *
+ * @example toCamelCase('a-string') => 'AString'
+ *
+ * @param string
+ *
+ * @return {string}
+ */
 module.exports.toCamelCase = (string) => {
     return string.replace(/\b(\w)/g, (match, capture) => {
         return capture.toUpperCase();
     }).replace(/\s+/g, '');
-}
+};
