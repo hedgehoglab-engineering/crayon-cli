ef0fbaec78a028694dd4ac8be7a041e438575e97
Sam Turrell <sam.turrell@netsells.co.uk>
2018-04-13 00:07:58 +0100

    travis Removes debug

--- tests/cli-runs.test.js
+++ tests/cli-runs.test.js
@@ -3,9 +3,6 @@ const { version } = require('../package');
 
 test('cli runs', () => {
     return exec('crayon').then((content) => {
-        console.log(content)
         return expect(content).toMatch(new RegExp(`crayon ${version}`));
-    }).catch((error) => {
-        console.log(error)
     });
 });
