2c0a40fcd4a1b1aa3c2423f560fb100fe52b4fed
Sam Turrell <sam.turrell@netsells.co.uk>
2018-04-13 00:03:38 +0100

    travis Debug

--- tests/cli-runs.test.js
+++ tests/cli-runs.test.js
@@ -5,5 +5,7 @@ test('cli runs', () => {
     return exec('crayon').then((content) => {
         console.log(content)
         return expect(content).toMatch(new RegExp(`crayon ${version}`));
+    }).catch((error) => {
+        console.log(error)
     });
 });
