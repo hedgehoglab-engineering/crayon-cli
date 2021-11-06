c8a68382098c1505a72c43b37b90a1bf8a490d06
Sam Turrell <sam.turrell@netsells.co.uk>
2021-11-06 23:39:41 +0000

    Logger and logo

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const { program } = require('@caporal/core');
 const glob = require('glob');
 const commands = glob.sync(`${ __dirname }/commands/*/index.js`);
+const logger = require('./logger');
+const fs = require('fs');
+const logo = fs.readFileSync('./utils/logo.txt', 'utf-8');
+
+program.description(`a frontend CLI tool for bootstrapping and scaffolding frontend applications. \n ${ logo }`);
+program.logger(logger);
 
 // Register each command
 commands.forEach(require);
